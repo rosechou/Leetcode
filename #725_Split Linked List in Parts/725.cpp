b86b78feb2ee8b00ea9c02f6fb104265430a64ee
rosechou <jcchou100@gmail.com>
2020-10-15 10:59:14 +0800

    Create 725.cpp

---
+++ #725_Split Linked List in Parts/725.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+        vector<ListNode*> list;
+        int length = 0;
+        ListNode* ptr = root;
+        while(ptr!= NULL)
+        {
+            length++;
+            ptr = ptr->next;
+        }
+        int sum = length/k;
+        int max = length%k;
+        int max2 = max;
+        while(max!=0)
+        {
+            ListNode* parti = root;
+            for(int i=0;i<sum;i++)
+            {   
+                parti=parti->next;
+            }
+            list.push_back(root);
+            root = parti->next;
+            parti->next = NULL;
+            max--;
+        }
+        while((k-max2)>0)
+        {
+            ListNode* parti = root;
+            if(parti==NULL)
+            {
+                list.push_back(NULL);
+                max2++;
+            }
+            else
+            {
+                for(int i=0;i<sum-1;i++)
+                {   
+                    parti=parti->next;
+                }
+                list.push_back(root);
+                root = parti->next;
+                parti->next = NULL;
+                max2++;
+            }
+           
+        }
+        return list;
+        
+    }
+};
