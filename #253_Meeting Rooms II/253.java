418be1b264fe3d54415c2ed65a88a6f35bb3c3b7
rosechou <jcchou100@gmail.com>
2021-09-29 14:19:17 -0500

    Create 253.java

---
+++ #253_Meeting Rooms II/253.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int maxNum = max(intervals);
+        // int minNum = min(intervals);
+        int[] roomsTime = new int[maxNum+1];
+        for(int i = 0; i < intervals.length; i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            for(int j = start; j <= end; j++){
+                roomsTime[j]++;
+            }
+        }
+        return maxOneDim(roomsTime);
+    }
+    
+    public static int max(int[][] t) {
+    int maximum = t[0][0];   // start with the first value
+    for (int i=0; i<t.length; i++) {
+        for(int j = 0; j<t[i].length;j++){
+            if (t[i][j] > maximum) {
+            maximum = t[i][j];   // new maximum
+            }
+        } 
+    }
+    return maximum;
+    }//end method max
+    
+    public static int maxOneDim(int[] t) {
+    int maximum = t[0];   // start with the first value
+    for (int i=1; i<t.length; i++) {
+        if (t[i] > maximum) {
+            maximum = t[i];   // new maximum
+        }
+    }
+    return maximum;
+    }//end method max
+
+}
